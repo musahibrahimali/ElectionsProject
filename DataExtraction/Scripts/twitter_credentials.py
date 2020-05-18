1dac76ee1662d4be9edc89e067f6cd3c91cc8293
MIA-GH <51687157+MIA-GH@users.noreply.github.com>
2020-05-18 11:09:00 +0100

    Update twitter_credentials.py

--- DataExtraction/Scripts/twitter_credentials.py
+++ DataExtraction/Scripts/twitter_credentials.py
@@ -3,9 +3,9 @@ Ths file contains the twitter credentials from the twitter application
 in the developer console
 """
 
-ACCESS_TOKEN = "2242232880-7OKjM76NWIEW54UhykDhfcwESdPwJkGsG7mzrks"
-ACCESS_TOKEN_SECRET = "rzTz8l7ML2v5gVhJjpM7ncJ7TmTZrRCM2SkLg9TYbFBpG"
-CONSUMER_KEY = "VYU7BstMhgWvcpNiFWmcFFzLT"
-CONSUMER_SECRET = "qapAOetQEmdfawFRCHF4M43cU5aj0SLdQZ5heHDQYPwHaMMSRa"
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # # # please note that the above token has been revoked and wont work now
